826d2810a332c2747d95b2ded267637273a25092
Collin Reeser <collin.reeser@gmail.com>
2022-02-13 02:41:09 +0000

    Pull in googletest

--- Makefile
+++ Makefile
@@ -1,5 +1,29 @@
+.PHONY: clean init
 
-all: pathfinding
+all: pathfinding test_unit
+
+init:
+	git submodule init
+	git submodule update
+	cd submodules/entt/build
+	cmake .. -DENTT_BUILD_DOCS=ON -DENTT_BUILD_TESTING=ON
+	make
+	make test
+	cd ../../googletest
+	mkdir build
+	cd build
+	cmake ..
+
+clean:
+	rm -f pathfinding
+	rm -f test_unit
+
+includes = -I submodules/entt/src -I submodules/googletest/googletest/include
+libs = -L submodules/googletest/build/lib
+links = -lgtest -lpthread
 
 pathfinding: pathfinding.cpp
-	g++ -Werror -Wall -I submodules/entt/src -std=c++17 -O2 pathfinding.cpp -o pathfinding 
+	g++ -g -O2 -Werror -Wall $(includes) -std=c++17 pathfinding.cpp -o pathfinding
+
+test_unit: test_unit.cpp
+	g++ -g -O2 -Werror -Wall $(includes) -std=c++17 test_unit.cpp -o test_unit $(libs) $(links)
--- pathfinding.cpp
+++ pathfinding.cpp
@@ -40,40 +40,44 @@ public:
     friend Map;
 };
 
+uint32_t get_node_index(
+    const uint32_t x, const uint32_t y, const uint32_t width
+) {
+    return y * width + x;
+}
+
+std::pair<uint32_t, uint32_t> get_node_xy(
+    const uint32_t i, const uint32_t width
+) {
+    const uint32_t y = i / width;
+    const uint32_t x = i - (y * width);
+
+    return {x, y};
+}
+
 class Map {
 private:
     static inline std::random_device rd;
     static inline std::mt19937 gen {rd()};
 
+    std::vector<MapNode> nodes;
+
+public:
     // The x-coordinate range.
     const uint32_t width {64};
     // The y-coordinate range.
     const uint32_t height {32};
 
-    std::vector<MapNode> nodes;
-
-public:
     Map(const uint32_t width = 64, const uint32_t height = 32):
         width(width),
         height(height)
     {}
 
     Map(Map&& o) noexcept:
+        nodes(std::move(o.nodes)),
         width(o.width),
-        height(o.height),
-        nodes(std::move(o.nodes))
+        height(o.height)
     {}
-
-    uint32_t get_node_index(const uint32_t x, uint32_t y) const {
-        return y * width + x;
-    }
-
-    std::pair<uint32_t, uint32_t> get_node_xy(const uint32_t i) const {
-        const uint32_t y = i / width;
-        const uint32_t x = i - (y * width);
-
-        return {x, y};
-    }
 
     static Map gen_rand_map() {
         std::uniform_int_distribution<> rng(0, 5);
@@ -83,7 +87,7 @@ public:
         map.nodes.reserve(map.width * map.height);
 
         for (uint32_t i = 0; i < map.nodes.capacity(); ++i) {
-            const auto [x, y] = map.get_node_xy(i);
+            const auto [x, y] = get_node_xy(i, map.width);
 
             map.nodes.emplace_back(x, y, rng(Map::gen) > 4);
         }
@@ -91,18 +95,17 @@ public:
         return map;
     }
 
-    void debug_print_map() {
+    void draw_map_to_frame(std::vector<char> &tiles) const {
         for (uint32_t y = 0; y < height; ++y) {
             for (uint32_t x = 0; x < width; ++x) {
-                if (nodes[get_node_index(x, y)].blocking) {
-                    std::cout << "X";
+                const uint32_t i {get_node_index(x, y, width)};
+                if (nodes[i].blocking) {
+                    tiles[i] = 'X';
                 }
                 else {
-                    std::cout << ".";
+                    tiles[i] = '.';
                 }
             }
-
-            std::cout << std::endl;
         }
     }
 
@@ -118,7 +121,7 @@ public:
             x = rng_w(Map::gen);
             y = rng_h(Map::gen);
 
-            i = get_node_index(x, y);
+            i = get_node_index(x, y, width);
         } while (!nodes[i].blocking);
 
         return {x, y};
@@ -130,7 +133,48 @@ struct Pos {
     float y;
 };
 
-struct Mover {};
+struct Pather {};
+
+struct Frame {
+private:
+    const uint32_t width;
+    const uint32_t height;
+
+    std::vector<char> tiles;
+
+public:
+    Frame(
+        const Map &map, const entt::registry &registry,
+        const uint32_t view_width = 128, const uint32_t view_height = 128
+    ):
+        width(map.width),
+        height(map.height)
+    {
+        tiles.reserve(width * height);
+
+        map.draw_map_to_frame(tiles);
+
+        auto view = registry.view<const Pos>();
+
+        for (const auto &[entity, pos]: view.each()) {
+            const uint32_t i = get_node_index(
+                (uint32_t)pos.x, (uint32_t)pos.y, width
+            );
+
+            tiles[i] = 'O';
+        }
+    }
+
+    void draw_frame() {
+        for (uint32_t y = 0; y < height; ++y) {
+            for (uint32_t x = 0; x < width; ++x) {
+                std::cout << tiles[get_node_index(x, y, width)];
+            }
+
+            std::cout << std::endl;
+        }
+    }
+};
 
 int main(int argc, char** argv) {
     entt::registry registry;
@@ -146,9 +190,12 @@ int main(int argc, char** argv) {
 
         const auto entity = registry.create();
         registry.emplace<Pos>(entity, (float)x + 0.5f, (float)y + 0.5f);
+        registry.emplace<Pather>(entity);
     }
 
-    map.debug_print_map();
+    Frame frame(map, registry);
+
+    frame.draw_frame();
 
     return 0;
 }
